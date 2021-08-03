eceec23a011f2dc7a6d16da79f912dea595491e0
indrico <indrico.jowensen@gmail.com>
2021-08-03 10:38:21 +0700

    Completed

--- src/App.js
+++ src/App.js
@@ -4,13 +4,34 @@ import Button from './Components/Button';
 function App() {
   return (
     <div className="container">
-      <Button name="<Button />"/>
-      <Button name="&:hover, &:focus" backgroundColor="rgb(172,172,172)"/>
-      <Button name={`<Button variant="outline"/>`} variant="outline"/>
-      <Button name="&:hover, &:focus" backgroundColor="#EAEFFF" variant="outline"/>
-      <Button name={`<Button variant="text"/>`} variant="text"/>
-      <Button name="&:hover, &:focus" backgroundColor="#EAEFFF" variant="text"/>
-      <Button name={`<Button disableShadow/>`} />
+      <div className="two-columns">
+        <Button name="<Button />" />
+        <Button name="&:hover, &:focus" backgroundColor="rgb(172,172,172)" />
+        <Button name={`<Button variant="outline"/>`} variant="outline" />
+        <Button name="&:hover, &:focus" backgroundColor="#EAEFFF" variant="outline" />
+        <Button name={`<Button variant="text"/>`} variant="text" />
+        <Button name="&:hover, &:focus" backgroundColor="#EAEFFF" variant="text" />
+        <Button name={`<Button disableShadow/>`} disableShadow="true" />
+        <Button name={`<Button disabled/>`} disabled="disabled" />
+        <Button name={`<Button variant="text" disabled/>`} variant="text" disabled="disabled" />
+        <Button name={`<Button startIcon="local_grocery_store"/>`} startIcon="local_grocery_store" />
+        <Button name={`<Button endIcon="local_grocery_store"/>`} endIcon="local_grocery_store" />
+      </div>
+      <div className="three-columns">
+        <Button name={`<Button size="sm"/>`} size="sm"/>
+        <Button name={`<Button size="md"/>`} size="md"/>
+        <Button name={`<Button size="lg"/>`} size="lg"/>
+      </div>
+      <div className="four-columns">
+        <Button name={`<Button color="default"/>`} color="default"/>
+        <Button name={`<Button color="primary"/>`} color="primary"/>
+        <Button name={`<Button color="secondary"/>`} color="secondary"/>
+        <Button name={`<Button color="danger"/>`} color="danger"/>
+        <Button name="&:hover, &:focus" backgroundColor="rgb(172,172,172)"/>
+        <Button name="&nbsp;" backgroundColor="#0039CB" color="primary"/>
+        <Button name="&nbsp;" backgroundColor="#1C313A" color="secondary"/>
+        <Button name="&nbsp;" backgroundColor="#9A0007" color="danger"/>
+      </div>
     </div>
   );
 }
--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,14 +1,50 @@
-const Button = (props) => {
-    let style;
+import LocalGroceryStoreIcon from '@material-ui/icons/LocalGroceryStore';
 
-    style = {
+const Button = (props) => {
+    let customStyle, grid;
+
+    customStyle = {
         backgroundColor: props.backgroundColor,
     }
 
+    if (props.disableShadow === "true") {
+        customStyle = {
+            backgroundColor: "#3D5AFD",
+            color: "#fff"
+        }
+
+        grid = {
+            gridColumn: "1 / span 2"
+        }
+    }
+
+    if (props.size) {
+        let size = props.size;
+        let padding;
+
+        if (size === "sm") {
+            padding = "5px 10px";
+        } else if (size === "md") {
+            padding = "10px 20px";
+        } else if (size === "lg") {
+            padding = "15px 30px";
+        }
+
+        customStyle = {
+            backgroundColor: "#3D5AFD",
+            color: "#fff",
+            padding: padding
+        }
+    }
+
     return (
-        <div className="button-container">
+        <div className="button-container" style={grid}>
             <p>{props.name}</p>
-            <button className={props.variant} style={style}>Default</button>
+            <button className={props.variant || props.color} style={customStyle} disabled={props.disabled}>
+                {props.startIcon && <LocalGroceryStoreIcon fontSize="small" style={{marginRight: "5px"}} />}
+                {props.disabled ? "Disabled" : "Default"}
+                {props.endIcon && <LocalGroceryStoreIcon fontSize="small" style={{marginLeft: "5px"}}/>}
+            </button>
         </div>
     )
 }
